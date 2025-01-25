9b4e8297d9814d683310aabb3addb0fab3a056e2
roshnidaksha <groshnidaksha2@yahoo.com>
2025-01-25 18:02:50 +0800

    Add playGame method to Planit class
    Planit task management system has multiple features (echoing user input
    and managing tasks).
    
    Keeping all the logic in main method makes it hard to read.
    
    Creating a separate method for playing a game improves code readability.

--- src/main/java/Planit.java
+++ src/main/java/Planit.java
@@ -16,6 +16,8 @@ public class Planit {
             +------------------------------------------------------------+
             """;
 
+    static Scanner sc = new Scanner(System.in);
+
     /**
      * Prints a line for enhanced readability.
      */
@@ -27,8 +29,12 @@ public class Planit {
      * Display greeting message during start of new session
      */
     static void greet() {
-        System.out.println("What can I do for you?");
+        System.out.println("What can I do for you?\n");
+        System.out.println("1. Type \"planit\" or \"1\" to manage your tasks");
+        System.out.println("2. Type \"play\" or \"2\" to have a break and play games");
+        System.out.println("3. Type \"bye\" or \"3\" to quit the session");
         printSeperator();
+        System.out.print("> ");
     }
 
     /**
@@ -40,6 +46,32 @@ public class Planit {
     }
 
     /**
+     * Display game options to user and redirect to correct class
+     */
+    static void playGame() {
+        System.out.println("""
+                 o   \\ o /  _ o         __|    \\ /     |__        o _  \\ o /   o
+                /|\\    |     /\\   ___\\o   \\o    |    o/    o/__   /\\     |    /|\\
+                / \\   / \\   | \\  /)  |    ( \\  /o\\  / )    |  (\\  / |   / \\   / \\
+                """);
+        boolean exitGameMode = false;
+        while (!exitGameMode) {
+            System.out.println("Looking for something fun?");
+            System.out.println("1. Type \"parrot\" or \"1\" to play the parrot game! -- ");
+            System.out.println("2. Type \"bye\" or \"2\" to quit the session");
+            System.out.print("> ");
+            String userChoice = sc.nextLine();
+            if (userChoice.equalsIgnoreCase("parrot") || userChoice.equals("1")) {
+                Echo.echo();
+            } else if (userChoice.equalsIgnoreCase("bye") || userChoice.equals("2")) {
+                exitGameMode = true;
+            } else {
+                System.out.println("Please enter a valid option!");
+            }
+        }
+    }
+
+    /**
      * The main method serves as the entry point for the program.
      *
      * @param args Command-line arguments
@@ -47,14 +79,19 @@ public class Planit {
     public static void main(String[] args) {
         System.out.println(logo);
         System.out.println("          Welcome to Planit, task management system!\n");
-        greet();
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Looking for something fun?");
-        System.out.print("Type \"parrot\" to play the parrot game! -- ");
-        String userChoice = sc.nextLine();
-        if (userChoice.equalsIgnoreCase("parrot")) {
-            Echo.echo();
+        boolean exitPlanit = false;
+        while (!exitPlanit) {
+            greet();
+            String userChoice = sc.nextLine();
+            if (userChoice.equalsIgnoreCase("play") || userChoice.equals("2")) {
+                playGame();
+            } else if (userChoice.equalsIgnoreCase("bye") || userChoice.equals("3")) {
+                exitPlanit = true;
+            } else {
+                System.out.println("Please enter a valid option!");
+            }
+            printSeperator();
         }
 
         exit();
